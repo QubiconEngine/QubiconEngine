7a1d8466cc9f9dca868b30943de9e507aa67d1b2
Blinc <65546512+Blinc13@users.noreply.github.com>
2024-03-16 11:59:08 +0300

    right pin impl

--- core/qubicon_audio_server/src/lib.rs
+++ core/qubicon_audio_server/src/lib.rs
@@ -1,4 +1,4 @@
-use std::{ffi::{CStr, CString}, mem::MaybeUninit, pin::Pin};
+use std::{borrow::BorrowMut, ffi::{CStr, CString}, mem::MaybeUninit, pin::Pin};
 use libpulse_sys::*;
 
 pub mod error;
@@ -19,35 +19,39 @@ struct PulseData {
     mainloop_api: *const pa_mainloop_api,
 
     ctx: *mut pa_context,
-    ctx_state: pa_context_state_t
+    ctx_state: pa_context_state_t,
+
+    _ph: std::marker::PhantomPinned
 }
 
 impl PulseData {
-    // Type should be fixed in place. I.e pinned
-    pub unsafe fn init(&mut self, name: &CStr) -> Result<()> {
+    // rewrites all data whats inside without destructor
+    pub unsafe fn init(self: Pin<&mut Self>, name: &CStr) -> Result<()> {
+        let this = self.get_unchecked_mut();
+
         // set to some random value
         // this field should not contain some random data, because will be used in initialization process
-        self.ctx_state = pa_context_state_t::Unconnected;
+        this.ctx_state = pa_context_state_t::Unconnected;
 
-        self.mainloop = pa_mainloop_new();
-        self.mainloop_api = pa_mainloop_get_api(self.mainloop);
+        this.mainloop = pa_mainloop_new();
+        this.mainloop_api = pa_mainloop_get_api(this.mainloop);
 
-        self.ctx = pa_context_new(self.mainloop_api, name.as_ptr());
+        this.ctx = pa_context_new(this.mainloop_api, name.as_ptr());
 
-        pa_context_connect(self.ctx, core::ptr::null(), Default::default(), core::ptr::null());
-        pa_context_set_state_callback(self.ctx, Some(ctx_state_callback), (self as *mut Self).cast());
+        pa_context_connect(this.ctx, core::ptr::null(), Default::default(), core::ptr::null());
+        pa_context_set_state_callback(this.ctx, Some(ctx_state_callback), (this as *mut Self).cast());
 
         
         // yes, its busy loop
         // ctx_state is changed in callback
         #[allow(clippy::while_immutable_condition)]
-        while self.ctx_state != pa_context_state_t::Ready {
-            pa_mainloop_iterate(self.mainloop, 1, core::ptr::null_mut());
+        while this.ctx_state != pa_context_state_t::Ready {
+            pa_mainloop_iterate(this.mainloop, 1, core::ptr::null_mut());
             
-            if !pa_context_is_good(self.ctx_state) {
-                self.destroy_resources();
+            if !pa_context_is_good(this.ctx_state) {
+                this.destroy_resources();
 
-                return Err(error::Error::ContextConnectionFailed { ctx_state: self.ctx_state })
+                return Err(error::Error::ContextConnectionFailed { ctx_state: this.ctx_state })
             }
         }
 
@@ -87,7 +91,7 @@ impl AudioServer {
             }
         );
 
-        unsafe { data.init(&CString::new("test").unwrap()) }?;
+        unsafe { data.as_mut().init(&CString::new("test").unwrap()) }?;
 
         Ok ( Self { data } )
     }
