2b0d06d6cd8217ad443931728f0e2321567a4a24
Blinc <65546512+Blinc13@users.noreply.github.com>
2024-04-03 20:40:40 +0300

    Closer, but half float still dont work properly

--- core/misc/qubicon_short_floats/src/lib.rs
+++ core/misc/qubicon_short_floats/src/lib.rs
@@ -19,6 +19,7 @@ pub trait ShortFloat: From<f32> + Into<f32> {
     }
 }
 
+#[derive(PartialEq, Clone, Copy)]
 pub struct HalfF16 (u16);
 
 impl ShortFloat for HalfF16 {
@@ -43,14 +44,21 @@ impl ShortFloat for HalfF16 {
 
 impl From<f32> for HalfF16 {
     fn from(value: f32) -> Self {
+        let _dec = value.integer_decode();
         #[allow(clippy::transmute_float_to_int)]
         let value: u32 = unsafe { core::mem::transmute(value) };
 
+        let sign = value >> 31;
+        let exp_sign = value << 1 >> 31;
+        let exp = value << 1 >> 24;
+        let mantis = value & 0b0000_0000_0111_1111_1111_1111_1111_1111;
+
         let mut out = 0u16;
 
-        out |= ((value >> 31) as u16) << 15;
-        out |= ((value << 4 >> 26) as u16) << 10;
-        out |= ((value << 9 >> 21) as u16);
+        out |= (sign as u16) << 15;
+        out |= (exp_sign as u16) << 14;
+        out |= (exp as u16 & 0b1111) << 10;
+        out |= (mantis >> 13) as u16;
 
         Self ( out )
     }
@@ -58,17 +66,26 @@ impl From<f32> for HalfF16 {
 
 impl Into<f32> for HalfF16 {
     fn into(self) -> f32 {
+        let sign = self.sign();
+        let exp = self.exp();
+        let mantis = self.mantis();
+
+        let exp_sign = exp >> 4;
+        let exp = exp & 0b1111;
+
         let mut out = 0u32;
 
-        out |= (self.sign() as u32) << 31;
-        out |= (self.exp() as u32) << 20;
-        out |= (self.mantis() as u32) << 16;
+        out |= (sign as u32) << 31;
+        out |= (exp_sign as u32) << 30;
+        out |= ((exp & 0b1111) as u32) << 23;
+        out |= (mantis as u32) << 12;
 
         #[allow(clippy::transmute_int_to_float)]
         unsafe { core::mem::transmute(out) }
     }
 }
 
+#[derive(PartialEq, Clone, Copy)]
 pub struct BF16 (u16);
 
 impl ShortFloat for BF16 {
@@ -130,6 +147,14 @@ impl Into<f32> for BF16 {
 mod tests {
     use super::*;
 
+    fn check_stability<T: ShortFloat + PartialEq + Copy>() {
+        let num = T::from(-19.0);
+        let num_f32: f32 = num.into();
+        let num_r = T::from(num_f32);
+
+        assert!(num == num_r);
+    }
+
     #[test]
     fn bf16() {
         let f_32 = 10.0f32.powi(37);
@@ -138,6 +163,8 @@ mod tests {
         let f_32_c: f32 = f_16.into();
 
         println!("{f_32} {f_32_c}");
+
+        check_stability::<BF16>();
     }
 
     #[test]
@@ -145,10 +172,12 @@ mod tests {
         let f_32 = 15.0f32;
         let f_16 = HalfF16::from(f_32);
 
-        println!("{} {} {}", f_16.sign(), f_16.exp(), f_16.mantis());
-
         let f_32_c: f32 = f_16.into();
 
         println!("{f_32} {f_32_c}");
+
+        let _dec = f_32_c.integer_decode();
+
+        check_stability::<HalfF16>();
     }
 }
