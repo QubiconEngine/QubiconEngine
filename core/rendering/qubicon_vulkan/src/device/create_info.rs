52cb3623c3cb9ec507e532f87926eefdf1a902ac
Blinc <65546512+Blinc13@users.noreply.github.com>
2024-04-13 16:08:30 +0300

    Delete some garbage and start rewriting DeviceCreateInfo

--- core/rendering/qubicon_vulkan/src/device/create_info.rs
+++ core/rendering/qubicon_vulkan/src/device/create_info.rs
@@ -1,30 +1,10 @@
-use ash::vk::DeviceQueueCreateInfo as VkDeviceQueueCreateInfo;
-use crate::instance::physical_device::features::DeviceFeatures;
+pub struct DeviceCreateInfo {
+    pub queue_families: Vec<QueueFamilyUsage>,
+    pub extensions
+}
 
-#[derive(Debug, Clone, Copy, Default, PartialEq, Hash)]
+
 pub struct QueueFamilyUsage {
-    pub family_index: u32,
-    pub queue_count: u32,
-    // TODO: Queue priorities
-}
-
-impl From<QueueFamilyUsage> for VkDeviceQueueCreateInfo {
-    fn from(value: QueueFamilyUsage) -> Self {
-        VkDeviceQueueCreateInfo {
-            queue_family_index: value.family_index,
-            queue_count: value.queue_count,
-            p_queue_priorities: core::ptr::null(),
-
-            ..Default::default()
-        }
-    }
-}
-
-#[derive(Debug, Default, Clone, Copy, PartialEq, Eq, Hash)]
-pub struct DeviceCreateInfo<T: Into<Box<[QueueFamilyUsage]>> = Vec<QueueFamilyUsage>> {
-    pub features: DeviceFeatures,
-    pub queues: T,
-
-    #[cfg(feature = "windowing")]
-    pub enable_swapchain: bool
+    pub family_idx: u32,
+    pub queues: Vec<f32>
 }
