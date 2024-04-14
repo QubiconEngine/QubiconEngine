818f3452d5bc29b2a7220460ae91b54ba9ce9c00
Blinc <65546512+Blinc13@users.noreply.github.com>
2024-04-14 11:17:19 +0300

    Impl BitXor for DeviceFeatures

--- core/rendering/qubicon_vulkan/src/instance/physical_device/features.rs
+++ core/rendering/qubicon_vulkan/src/instance/physical_device/features.rs
@@ -291,6 +291,8 @@ impl core::ops::BitAnd for DeviceFeatures {
             variable_multisample_rate,
             inherited_queries
         );
+
+        self
     }
 }
 
@@ -359,5 +361,77 @@ impl core::ops::BitOr for DeviceFeatures {
             variable_multisample_rate,
             inherited_queries
         );
+
+        self
+    }
+}
+
+impl core::ops::BitXor for DeviceFeatures {
+    type Output = Self;
+
+    fn bitxor(mut self, rhs: Self) -> Self::Output {
+        bit_op!(
+            self,
+            rhs,
+            ^,
+
+            robust_buffer_access,
+            full_draw_index_uint32,
+            image_cube_array,
+            independent_blend,
+            geometry_shader,
+            tessellation_shader,
+            sample_rate_shading,
+            dual_src_blend,
+            logic_op,
+            multi_draw_indirect,
+            draw_indirect_first_instance,
+            depth_clamp,
+            depth_bias_clamp,
+            fill_mode_non_solid,
+            depth_bounds,
+            wide_lines,
+            large_points,
+            alpha_to_one,
+            multi_viewport,
+            sampler_anisotropy,
+            texture_compression_etc2,
+            texture_compression_astc_ldr,
+            texture_compression_bc,
+            occlusion_query_precise,
+            pipeline_statistics_query,
+            vertex_pipeline_stores_and_atomics,
+            fragment_stores_and_atomics,
+            shader_tessellation_and_geometry_point_size,
+            shader_image_gather_extended,
+            shader_storage_image_extended_formats,
+            shader_storage_image_multisample,
+            shader_storage_image_read_without_format,
+            shader_storage_image_write_without_format,
+            shader_uniform_buffer_array_dynamic_indexing,
+            shader_sampled_image_array_dynamic_indexing,
+            shader_storage_buffer_array_dynamic_indexing,
+            shader_storage_image_array_dynamic_indexing,
+            shader_clip_distance,
+            shader_cull_distance,
+            shader_float64,
+            shader_int64,
+            shader_int16,
+            shader_resource_residency,
+            shader_resource_min_lod,
+            sparse_binding,
+            sparse_residency_buffer,
+            sparse_residency_image2_d,
+            sparse_residency_image3_d,
+            sparse_residency2_samples,
+            sparse_residency4_samples,
+            sparse_residency8_samples,
+            sparse_residency16_samples,
+            sparse_residency_aliased,
+            variable_multisample_rate,
+            inherited_queries
+        );
+
+        self
     }
 }
