004b21f3717ae27dec3a33090b646fc8d1a77ba5
Blinc <65546512+Blinc13@users.noreply.github.com>
2024-04-16 23:02:07 +0300

    Add allocation info structure

--- core/rendering/qubicon_vulkan/src/memory/mod.rs
+++ core/rendering/qubicon_vulkan/src/memory/mod.rs
@@ -1,6 +1,25 @@
 use std::sync::Arc;
 
-use crate::{ device::Device, instance::physical_device::DeviceSize };
+use crate::{ device::Device, instance::physical_device::{ DeviceSize, PhysicalDevice } };
+
+pub struct AllocationInfo {
+    pub size: DeviceSize,
+    pub memory_type: u32
+}
+
+impl AllocationInfo {
+    pub fn validate(&self, device: &PhysicalDevice) {
+        if self.size == 0 {
+            panic!("allocation size shouldnt be zero");
+        }
+
+        let memory_types = &device.memory_properties().memory_types;
+
+        if memory_types.len() < self.memory_type {
+            panic!("no memory type with index {} exist for this device", self.memory_type);
+        }
+    }
+}
 
 pub struct MemoryObject {
     device: Arc<Device>,
