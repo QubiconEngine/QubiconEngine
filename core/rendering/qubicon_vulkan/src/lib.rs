8842eeb15e427f868212a8b46057cc2704808e62
Blinc <65546512+Blinc13@users.noreply.github.com>
2024-04-19 22:09:04 +0300

    Comment every module what wasnt rewritten

--- core/rendering/qubicon_vulkan/src/lib.rs
+++ core/rendering/qubicon_vulkan/src/lib.rs
@@ -1,18 +1,18 @@
 pub use error::Error;
 pub use instance::Instance;
 
-pub mod queue;
+//pub mod queue;
 pub mod device;
 pub mod memory;
-pub mod resources;
-pub mod descriptors;
+//pub mod resources;
+//pub mod descriptors;
 pub mod instance;
-pub mod commands;
-pub mod shaders;
+//pub mod commands;
+//pub mod shaders;
 pub mod error;
-pub mod sync;
+//pub mod sync;
 
-#[cfg(feature = "windowing")]
-pub mod swapchain;
-#[cfg(feature = "windowing")]
-pub mod surface;
+// #[cfg(feature = "windowing")]
+// pub mod swapchain;
+// #[cfg(feature = "windowing")]
+// pub mod surface;
