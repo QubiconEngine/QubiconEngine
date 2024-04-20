ec8e61f6bd0f31dc2038515bbb4c7362b23e1923
Blinc <65546512+Blinc13@users.noreply.github.com>
2024-04-20 14:00:36 +0300

    Uncomment resources module

--- core/rendering/qubicon_vulkan/src/lib.rs
+++ core/rendering/qubicon_vulkan/src/lib.rs
@@ -4,7 +4,7 @@ pub use instance::Instance;
 //pub mod queue;
 pub mod device;
 pub mod memory;
-//pub mod resources;
+pub mod resources;
 //pub mod descriptors;
 pub mod instance;
 //pub mod commands;
