2d7a3544c499fe80cbc5747a8ab0b31b820ffaf2
Blinc <65546512+Blinc13@users.noreply.github.com>
2024-04-20 13:45:55 +0300

    Update create_logical_device example

--- core/rendering/qubicon_vulkan/examples/create_logical_device.rs
+++ core/rendering/qubicon_vulkan/examples/create_logical_device.rs
@@ -1,30 +1,33 @@
-use qubicon_vulkan::device::create_info::DeviceCreateInfo;
-use qubicon_vulkan::{Instance, instance::physical_device::properties::DeviceType, device::create_info::QueueFamilyUsage};
+use std::collections::HashMap;
+
+use qubicon_vulkan::{ Instance, device::DeviceCreateInfo, instance::{ AppId, InstanceCreateInfo, Version } };
+
+const APP_ID: AppId = AppId {
+    app_version: Version::new(0, 1, 0, 0),
+    engine_version: Version::new(0, 1, 0, 0),
+
+    vulkan_version: Version::new(0, 1, 0, 0),
+
+    app_name: "Device enumerator",
+    engine_name: "Shit"
+};
 
 fn main() {
-    let instance = Instance::create(&Default::default())
-        .expect("Failed to create instance");
+    let instance = Instance::new(&InstanceCreateInfo { app_id: APP_ID })
+        .expect("failed to create instance");
 
-    let queue_usage = [
-        QueueFamilyUsage {
-            family_index: 0,
-            queue_count: 1
+
+    let device = instance.enumerate_devices()
+        .expect("failed to enumerate devices")
+        .next()
+        .expect("no devices found");
+
+
+    let _logical_device = device.create_logical_device(
+        DeviceCreateInfo {
+            // no queues used
+            queue_families: HashMap::new(),
+            features: Default::default()
         }
-    ];
-
-    let device = instance
-        .enumerate_devices()
-        .expect("Failed to enumerate physical devices")
-        .find(| d | d.get_properties().device_type == DeviceType::IntegratedGpu)
-        .expect("No devices found")
-        .create_logical_device(DeviceCreateInfo {
-            features: Default::default(),
-            queues: queue_usage.to_vec(),
-
-            ..Default::default()
-        })
-        .expect("Failed to create logical device");
-    
-    let _queue = device.get_queue(0, 0)
-        .expect("Failed to create queue");
+    ).expect("failed to create logical device");
 }
