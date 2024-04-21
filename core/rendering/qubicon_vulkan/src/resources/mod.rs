90e83e93d02a5ecc12fa3a4c41fb7ea41157fb43
Blinc <65546512+Blinc13@users.noreply.github.com>
2024-04-21 10:29:50 +0300

    Add MemoryRequirements structure

--- core/rendering/qubicon_vulkan/src/resources/mod.rs
+++ core/rendering/qubicon_vulkan/src/resources/mod.rs
@@ -5,3 +5,25 @@ mod buffer;
 mod image_view;
 mod buffer_view;
 mod resource_factory;
+
+
+use crate::instance::physical_device::DeviceSize;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub struct MemoryRequirements {
+    pub size: DeviceSize,
+    pub alignment: DeviceSize,
+
+    pub memory_types: bitvec::BitArr!(for 32, in u32)
+}
+
+impl From<ash::vk::MemoryRequirements> for MemoryRequirements {
+    fn from(value: ash::vk::MemoryRequirements) -> Self {
+        Self {
+            size: value.size,
+            alignment: value.alignment,
+
+            memory_types: bitvec::array::BitArray::new(value.memory_type_bits)
+        }
+    }
+}
