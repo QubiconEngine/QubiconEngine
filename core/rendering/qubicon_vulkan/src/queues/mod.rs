df27e76430ec8f4d05c37b5536995624e92eac54
Blinc <65546512+Blinc13@users.noreply.github.com>
2024-05-01 17:34:35 +0300

    Add ways to get Queue from Device

--- core/rendering/qubicon_vulkan/src/queues/mod.rs
+++ core/rendering/qubicon_vulkan/src/queues/mod.rs
@@ -1,6 +1,6 @@
 use std::sync::Arc;
 
-use crate::device::{ Device, QueueFamilyIndex };
+use crate::{ error::VkError, device::{ Device, QueueFamilyIndex } };
 
 pub struct Queue {
     device: Arc<Device>,
@@ -14,6 +14,43 @@ pub struct Queue {
 impl Queue {
     pub(crate) unsafe fn as_raw(&self) -> ash::vk::Queue {
         self.queue
+    }
+
+    /// # Safety
+    /// There should be at least ***queue_index*** *+ 1* queues specified in [QueueFamilyUsage]
+    /// with **family_index** in [DeviceCreateInfo] during device creation
+    /// 
+    /// [QueueFamilyUsage]: crate::device::QueueFamilyUsage
+    /// [DeviceCreateInfo]: crate::device::DeviceCreateInfo
+    pub unsafe fn new_unchecked(device: Arc<Device>, family_index: QueueFamilyIndex, queue_index: u32) -> Self {
+        let queue = device.as_raw().get_device_queue(
+            family_index,
+            queue_index
+        );
+
+        Self {
+            device,
+            
+            family_index,
+            queue_index,
+
+            queue
+        }
+    }
+
+    /// If **family_index** or **queue_index** is invalid, will return [VkError]::[InititalizationFailed]
+    /// 
+    /// [VkError]: crate::error::VkError
+    /// [InititalizationFailed]: crate::error::VkError::InitializationFailed
+    pub fn new(device: Arc<Device>, family_index: QueueFamilyIndex, queue_index: u32) -> Result<Self, VkError> {
+        let usage = device.queue_families().get(&family_index)
+            .ok_or(VkError::InitializationFailed)?;
+
+        if queue_index as usize >= usage.queues.len() {
+            return Err( VkError::InitializationFailed );
+        }
+        
+        Ok( unsafe { Self::new_unchecked(device, family_index, queue_index) } )
     }
 
     pub fn device(&self) -> &Arc<Device> {
